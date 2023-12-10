9301f7b78cdcd9279cdd025a04c022764d005813
mihvlad08 <mihvlad08@gmail.com>
2023-12-10 19:20:08 +0200

    added buttons for logins, register and routes

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,30 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
 Route::get('/', function () {
     return view('welcome');
+});
+
+
+/* User routes */
+Route::get('/register', function () {
+    echo 1;
+});
+Route::post('/register', function () {
+    echo 1;
+});
+
+Route::get('/login', function () {
+    echo 1;
+});
+Route::post('/login', function () {
+    echo 1;
+});
+
+/* Admin routes */
+Route::get('/loginAdmin', function () {
+    echo 1;
+});
+Route::post('/loginAdmin', function () {
+    echo 1;
 });
